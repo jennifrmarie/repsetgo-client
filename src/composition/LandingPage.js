14604a309d960df8bd80b009b49b25c78cb32e0c
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-03-06 21:05:36 -0500

    added landing page

--- src/composition/LandingPage.js
+++ src/composition/LandingPage.js
@@ -5,10 +5,25 @@ import './LandingPage.css'
 export default class LandingPage extends Component {
     render() {
         return (
-            <div class="login-screen">
-                <span>This is where a login screen will go...</span>
-                <Link to="/dashboard">CLICK TO ENTER!</Link>
+            <div className="login">
+            <h1>Reppy, Set, GO!</h1>
+            <h2 className="below__head">(but do you, bro?)</h2>
+            <div className="login__form">
+              <form>
+                <div className="login__form__credentials">
+                  <input type="text" placeholder="Username" name="username" id="username"/>
+                  <input type="password" placeholder="Password" name="password" id="password"/>
+                </div>
+                <div className="login__form__controls">
+                  <div className="login__form__remember">
+                    <input type="checkbox" name="rememberMe" id="rememberMe"/>
+                    <label htmlFor="rememberMe">Remember me</label>
+                  </div>
+                  <Link to='/dashboard'>Submit</Link>
+                </div>
+              </form>
             </div>
+          </div>
         )
     }
 }
