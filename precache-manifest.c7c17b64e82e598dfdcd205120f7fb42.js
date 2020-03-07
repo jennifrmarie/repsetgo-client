17feec995d1317894641e30023317b963c0c5056
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-03-06 19:42:35 -0500

    Updates

---
+++ precache-manifest.c7c17b64e82e598dfdcd205120f7fb42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c505c380c093c635b4c1a426fa747c",
+    "url": "/repsetgo-client/index.html"
+  },
+  {
+    "revision": "aa7636183bc1506e0165",
+    "url": "/repsetgo-client/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "fa987656731eb25a6fbf",
+    "url": "/repsetgo-client/static/css/main.62f0518c.chunk.css"
+  },
+  {
+    "revision": "aa7636183bc1506e0165",
+    "url": "/repsetgo-client/static/js/2.1eea5526.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/repsetgo-client/static/js/2.1eea5526.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa987656731eb25a6fbf",
+    "url": "/repsetgo-client/static/js/main.b0bcd8eb.chunk.js"
+  },
+  {
+    "revision": "6963e0f3f71a026efff4",
+    "url": "/repsetgo-client/static/js/runtime-main.1445931b.js"
+  }
+]);
