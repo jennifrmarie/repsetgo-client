58e9a643420b773713032f8755b49ffbd16da708
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-03-07 08:27:34 -0500

    added input function

--- src/composition/LandingPage.js
+++ src/composition/LandingPage.js
@@ -3,6 +3,25 @@ import { Link } from 'react-router-dom'
 import './LandingPage.css'
 
 export default class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+        this.handleUsernameInput.bind(this);
+        this.handlePasswordInput.bind(this);
+    }
+    handleUsernameInput(e) {
+        this.setState({
+            username: e.target.value,
+        })        
+    }
+    handlePasswordInput(e) {
+        this.setState({
+            password: e.target.value,
+        })        
+    }
     render() {
         return (
             <div className="login">
@@ -11,8 +30,10 @@ export default class LandingPage extends Component {
             <div className="login__form">
               <form>
                 <div className="login__form__credentials">
-                  <input type="text" placeholder="Username" name="username" id="username"/>
-                  <input type="password" placeholder="Password" name="password" id="password"/>
+                  <input type="text" placeholder="Username" value={this.state.username}
+                        onChange={this.handleUsernameInput.bind(this)} name="username" id="username"/>
+                  <input type="password" placeholder="Password" value={this.state.password}
+                        onChange={this.handlePasswordInput.bind(this)} name="password" id="password"/>
                 </div>
                 <div className="login__form__controls">
                   <div className="login__form__remember">
